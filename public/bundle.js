5b986582c6e57dcde2e7543ade3bb17c22cdcab8
Simon Nguyen <simonnguyen3054@gmail.com>
2019-04-30 17:47:05 -0700

    refactor Routes.js to be load data on the server

--- public/bundle.js
+++ public/bundle.js
@@ -27524,8 +27524,6 @@ var _react = __webpack_require__(1);
 
 var _react2 = _interopRequireDefault(_react);
 
-var _reactRouterDom = __webpack_require__(32);
-
 var _Home = __webpack_require__(97);
 
 var _Home2 = _interopRequireDefault(_Home);
@@ -27536,14 +27534,14 @@ var _UsersList2 = _interopRequireDefault(_UsersList);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-exports.default = function () {
-  return _react2.default.createElement(
-    "div",
-    null,
-    _react2.default.createElement(_reactRouterDom.Route, { exact: true, path: "/", component: _Home2.default }),
-    _react2.default.createElement(_reactRouterDom.Route, { exact: true, path: "/users", component: _UsersList2.default })
-  );
-};
+exports.default = [{
+  path: "/",
+  component: _Home2.default,
+  exact: true
+}, {
+  path: "/users",
+  component: _UsersList2.default
+}];
 
 /***/ }),
 /* 97 */
